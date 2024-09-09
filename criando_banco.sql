678c678f1daa5d587977e013413a73c946af6995
Leandro Junio <155511363+Junimdarua18@users.noreply.github.com>
2024-09-09 15:33:37 -0300

    Create criando_banco.sql

---
+++ criando_banco.sql
@@ -0,0 +1,255 @@
+/*
+SENAC DF - CEP Jessé Freire
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares Prof Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com MySQ1
+Projeto - Clinica Odontológica OdontoVida */
+-- 1. Criação do Arquivo/Banco de Dados
+create database OdontoVida;
+-- 2. Acessando Arquivo/Banco de Dados
+use OdontoVida;
+
+
+
+-- 3 Criando Tablelas
+CREATE TABLE Paciente (
+paciente_id INT AUTO_INCREMENT PRIMARY KEY, 
+nome VARCHAR(100) NOT NULL,
+data_nascimento DATE NOT NULL,
+telefone VARCHAR(20),
+endereco TEXT,
+email VARCHAR(100)
+);
+  
+CREATE TABLE Dentista (
+dentista_id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+especialidade VARCHAR(50),
+telefone VARCHAR(20),
+email VARCHAR(100),
+);
+
+CREATE TABLE Consulta (
+consulta_id INT AUTO_INCREMENT PRIMARY KEY,
+paciente_id INT,
+dentista_id INT,
+data_consulta DATETIME,
+FOREIGN KEY (paciente_id) REFERENCES Paciente (paciente_id), 
+FOREIGN KEY (dentista_id) REFERENCES Dentista (dentista_id)
+);
+
+
+
+CREATE TABLE Tratamento (
+tratamento_id INT AUTO_INCREMENT PRIMARY KEY,
+consulta_id INT,
+descricao TEXT,
+data inicio DATE,
+data_fim DATE,
+FOREIGN KEY (consulta_id) REFERENCES Consulta (consulta_id)
+);
+
+CREATE TABLE Procedimento (
+procedimento_id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+custo DECIMAL(18, 2) NOT NULL
+);
+  
+CREATE TABLE Tratamento_Procedimento (
+tratamento_id INT,
+procedimento_id INT,
+quantidade INT DEFAULT 1,
+PRIMARY KEY (tratamento_id, procedimento_id),
+FOREIGN KEY (tratamento_id) REFERENCES Tratamento (tratamento_id),
+FOREIGN KEY (procedimento_id) REFERENCES Procedimento (procedimento_id)
+);
+  
+CREATE TABLE Pagamento (
+pagamento_id INT AUTO_INCREMENT PRIMARY KEY,
+tratamento_id INT,
+valor DECIMAL(18, 2) NOT NULL,
+data_pagamento DATE,
+FOREIGN KEY (tratamento_id) REFERENCES Tratamento (tratamento_id)
+);
+
+
+/*SENAC DF - CEP Jessé Freire
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares
+Profe Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com Mysol
+projeto - Clinica Odontológica OdontoVida*/
+INSERT INTO Paciente (nome, data_nascimento, telefone, endereco, email) VALUES
+('Ana Silva', '1985-84-15', '11987654321', 'Rua das Flores, 123, são Paulo','ana.silva@email.com'),
+('Carlos Oliveira', '1990-86-30', '21987654321', 'Avenida Paulista, €56, São Paulo', 'carlos.oliveira@email.com'),
+('Fernanda Costa', '1982-12-12', '31987654321', 'Rua das Acácias, 789, Belo Horizonte', 'fernanda.costa@email.com'),
+('João Santos', '1978-83-21', '41987654321', 'Praça da Liberdade, 101, Belo Horizonte', 'joao.santos@email.com'),
+('Maria Souza', '1995-89-10', '51987654321', 'Rua do Mercado, 282, Rio de Janeiro', 'maria.souza@email.com'),
+('Pedro Lima', '1988-07-25', '61987654321', 'Rua das Palmeiras, 303, Rio de Janeiro','pedro.lima@email.com'),
+('Paula Ferreira', '1992-11-11', '71987654321', 'Rua dos Pinheiros, 404, Porto Alegre','paula. ferreira@email.com'),
+('Roberto Almeida', '1986-05-05', '81987654321', 'Avenida dos Anjos, 505, Porto Alegre', 'roberto.almeida@email.com'),
+('Sofia Martins', '1984-01-20', '91987654321', 'Rua das Orquídeas, 686, Curitiba', 'sofia.martins@email.com'),
+('Tiago Pereira', '1991-83-15', '11976543210', 'Rua dos Lírios, 707, Curitiba', 'tiago.pereira@email.com');
+INSERT INTO Dentista (nome, especialidade, telefone, email), VALUES
+('Dr: João Mendes', 'Ortodontia', '11912345678', 'joao.mendes@email.com'),
+('Dra, Maria Oliveira', 'Endodontia', '21912345678','maria.oliveira@email.com' ),
+('Dr. Pedro siiva', 'Periodontia', '31912345678', 'pedro.silva@email.com' ),
+('Dra. Fernanda Santos','Odontopediatria', '41912345678', 'fernanda.santos@email.com'),
+('or, Paulo Lima', 'Implantes', '51912345678', 'paulo.1imademail.com'),
+('Dra. Juliana Costa','Estética', '61912345678', 'juliana.costa@email.com'),
+('Dr. Ricardo Almeida', 'Cirurgia Oral', '71912345678', 'ricardo.almeida@email.com'),
+('Dra, Luana Martins', 'Clínica Geral', '81912345678', 'luana.martins@email.com'),
+('Dr, Gabriel Pereira', 'Prótese Dentária', '91912345678', 'gabriel.pereira@email.com'),
+('Dra. Laura Ferreira', 'Radiologia', '11923456789', 'laura.ferreira@email.com');
+
+
+INSERT INTO Consulta (paciente_id, dentista_id, data_consulta) VALUES
+(1, 1, '2024-09-10 10:00:00'),
+(2, 2, '2024-09-11 11:00:00'),
+(3, 3, '2024-09-12 09:00:00'),
+(4, 4, '2024-09-13 14:00:00'),
+(5, 5,'2024-09-14 15:00:00'),
+(6, 6, '2024-09-15 08:00:00'),
+(7, 7, '2024-09-16 13:00:00'),
+(8, 8,'2024-09-17 16:00:00'),
+(9, 9, '2024-09-18 12:00:90')，
+(10, 10, '2024-09-19 17:00:00');
+INSERT INTO Tratamento (consulta_id, descricao, data _inicio, data_fim) VALUES
+(1, 'Limpeza e Polimento', '2024-09-10','2024-09-10'),
+(2, 'Tratamento de Canal', '2024-09-11', '12024-89-253'),
+(3,'Aplicacao de Fluaris', '2024-09-12','12024-99-121'(,
+(4, 'Extracia de Dente'， '2024-09-13', '12024-99-13'),
+(5,'Colocação de Aparelho', '2024-09-14' '2024-12-14'),
+(6,'Implante Dentánio', '2024-09-15', '2024-10-15'),
+(7,'Clareamento Dental','2024-09-16', '2024-09-16'),
+(8,'Prootese Parcial','12024-89-17', '2024-10-17'),
+(9,'Tratamento de Gengival', '12024-09-18', '12024-09-25'),
+(13,'Reparo de Prótese', '2024-09-19', '2024-09-19');
+
+
+INSERT INTO Procedimento (nome, custo) VALUES
+('Limpeza Dental', 150.00),
+('Tratamento de Canal', 800.00),
+('Aplicação de Flúor', 100.00),
+('Extração de Dente', 200.00),
+('Colocação de Aparelho', 1500.00),
+('Implante Dentário', 2000.90),
+('Clareamento Dental', 500.00),
+('Protese Parcial', 1200.00),
+('Tratamento de Gengiva',308.08),
+('Reparo de Protese', 258.08);
+INSERT INTO Procedimento (nome, custo) VALUES
+('Limpeza Dental', 150.90),
+('Tratamento de Canal', 800.00),
+('Aplicação de Fiúor',100,00),
+('CExtracio de Dente',200.00）,
+('Colocação de Aparelho', 1500.00),
+('Inplante Denterig', 2000.60),
+('CLaneamento Dental', 500.00),
+('Prootese Parcel', 1200.90),
+('Tratamento de Gengtva', 300.00),
+('Reparo de Protese',250. 00)
+
+
+INSERT INTO Tratamento_Procedimento (tratamento_id, procedimento_id, quantidade) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 4, 1),
+(5, 5, 1),
+(6, 6, 1),
+(7, 7, 1）,
+(8, 8, 1),
+(9, 9, 1),
+(10, 10, 1);
+INSERT INTO Pagamento (tratamento_id, valor, data_pagamento) VALUES 
+(1, 150.00, '2024-09-10'),
+(2, 800.00,'2024-99-25'),
+(3, 100.00,'2024-09-12'),
+(4, 200.00,'2024-09-13'),
+(5, 1500.00,'12024-12-14'),
+(6, 2000.00,'12024-10-15'),
+(7, 500.00,'2024-09-1612'),
+(8, 1200.00,'12824-19-17923'),
+(9, 300.00,'2024-09-25'),
+(10, 250.00,'2024-09-19');
+
+
+
+/*
+SENAC DF - CEP Dessé Freire
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares
+Profe Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com MysQl
+Projeto - Clinica Odontológica OdontoVida */
+-- Consultas e joins
+/*
+Esta consulta lista os detalhes das consultas, incluindo o nome do paciente e o nome do dentista responsável por cada consulta.
+*/
+SELECT
+c.onsulta_id,
+p.nome As paciente _nome,
+d.nome AS dentista nome,
+c.data_consuLta
+FROM
+Consulta c
+iNNER JOIN
+Paciente p oN c.paciente_id = p.paciente_id
+iNNER JOIN
+Dentista d ON c.dentista_id = d-dentista_id;
+
+
+
+
+/*
+Esta consulta mostra todos os tratamentos realizados, junto com os procedimentos associados a cada tratamento e o custo total de cada procedimento.
+*/
+SELECT
+t.tratamento_id,
+t.descricao AS tratamento_descricao,
+p.nome AS procedimento_ nome,
+tp-quantidade,
+pr. custo,
+(tp. quantidade * pr.custo) AS custo_total
+FROM
+Tratamento t
+INNER JOIN
+Tratamento_Procedimento tp ON t.tratamento_id = tp. tratamento_id
+INNER JOIN
+Procedimento pr ON tp.procedimento_id = pr. procedimento_id;
+
+
+
+/*
+Esta consulta fornece informações sobre os pagamentos efetuados, incluindo o valor pago, a data do pagamento e a descrinção do tratamento assosiado
+*/
+SELECT
+p.pagamento_id,
+t.descricao AS tratamento_descricao,
+p.valor AS valor _pago,
+p.data_pagamento
+FROM
+Pagamento P
+INNER JOIN
+Tratamento t ON p.tratamento_id = t.tratamento_id;
+
+
+
+/*
+Sempre que um novo procedimento é inserido na tabela Tratamento_Procedimento, a trigger atualizará o custo total do tratamento correspondente na tabela Tratamento.
+*/
+DELIMITER //
+CREATE TRIGGER AtualizarCustoTratamento
+AFTER INSERT ON Tratamento_ Procedimento
+FOR EACH ROW
+BEGIN
+DECLARE custo_total DECIMAL (10, 2);
+-- Calcula o custo total do tratamento
+SELECT SUM(tp-quantidade * p-custo) INTO custo_total
+FROM Tratamento_ Procedimento tp
+JOIN Procedimento p ON tp-procedimento_id = p-procedimento_id
+WHERE tp.tratamento_id = NEW.tratamento_id;
+-- Atualiza o valor total do tratamento na tabela Tratamento
+UPDATE Tratamento
+SET valor_total - custo_total
+WHERE tratamento_id = NEW.tratamento_id;
+END//
+DELIMITER ;
